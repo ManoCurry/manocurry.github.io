c1d40579fcbdd0a9a7ed8e52c3b13b37798df57d
Jiseung Roh <jiseung.roh@gmail.com>
2017-11-09 20:36:09 +0900

    Deploy

---
+++ _next/20ac9bf2-79e4-493a-a891-753aad0e5242/page/projects/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/projects', function() {
+            var comp = module.exports=webpackJsonp([9],{799:function(e,t,u){e.exports=u(800)},800:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var l=u(1),r=n(l),o=u(31),a=(n(o),u(32)),d=n(a),f=function(){return r.default.createElement(d.default,null,r.default.createElement("h2",null,"index"))};t.default=f}},[799]);
+            return { page: comp.default }
+          })
+        
